5f80c39791efc93e57e767043dbf55ac763c2156
PJ Frias <peejfrias@gmail.com>
2018-02-01 14:20:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/' do
+    "<h1>Dynamic Routes Lab</h1>"
+  end
+  
   get '/reversename/:name' do
     "#{params[:name].reverse}"
   end
