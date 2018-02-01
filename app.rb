122440ee6771eb896e6797328b99137dcddf6caf
PJ Frias <peejfrias@gmail.com>
2018-02-01 14:38:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @count = params[:number].to_i
-    binding.pry
+    raise @count.inspect
     @count.times do
       "#{params[:phrase]}"
     end
